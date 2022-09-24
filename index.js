a2e391d422bee0d3752423748ebbc2f55a5b8dc4
Sheikh El-Moctar <sheikh.el.moctar@gmail.com>
2022-09-24 17:57:45 +0000

    complated

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = Express();
 const { db } = require("./util/admin");
 app.get("/", fetchData);
 // fetch data from firebase
-async function fetchData(req, res){
+function fetchData(req, res){
     const users = db.collection("users");
     try {
         users.get().then(snapshot=> {
@@ -17,6 +17,27 @@ async function fetchData(req, res){
     } catch (error) {
         return res.status(500).send({general: "there are error, try again."});
     }
+}
+async function addToDatabase(){
+    const data = {
+        name: "yeslem", 
+        lastname: "ahmed"
+    }
+    const usersCollection = await db.collection("users").doc(randomId(5)).create(data);
+}
+for (let index = 0; index < 200; index++) {
+    addToDatabase()
+}
+// to get random strings
+function randomId(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
 }
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, ()=> console.log("we are running in port "+ PORT));
